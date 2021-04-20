3a222ccf3240d34249dd8350a1d9d63ab0182657
rishivijayv <rishivijayv@gmail.com>
2021-04-19 21:12:43 -0400

    Added basic styling to username and profile nav options

--- src/User/User.js
+++ src/User/User.js
@@ -10,40 +10,70 @@ import Settings from './Settings/Settings';
 import UploadedImages from './UploadedImages/UploadedImages';
 import SavedImages from './SavedImages/SavedImages';
 import Navigation from '../Navigation/Navigation';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    mainContainer: {
+        margin: '20px',
+        fontFamily: theme.typography.fontFamily,
+    },
+    username: {
+        fontSize: '60px',
+        fontWeight: '800',
+        marginTop: '20px'
+    },
+    profileOptions: {
+        listStyleType: 'none',
+        padding: '0',
+        '& a': {
+            textDecoration: 'none',
+            fontWeight: '900',
+            fontSize: '22px'
+        },
+        '& li': {
+            display: 'inline-block',
+            marginRight: '20px'
+        }
+    }
+}));
 
 function User(){
+    const classes = useStyles();
+    
     const { username } = useParams();
     const match = useRouteMatch();
     return (
         <div>            
             <Navigation />
-            <h1>Home for {username}</h1>
-            <ul>
-                <li>
-                    <Link to={match.url}>Images</Link>
-                </li>
-                <li>
-                    <Link to={`${match.url}/labels`}> Labels </Link>
-                </li>
-                <li>
-                    <Link to={`${match.url}/settings`}> Settings </Link>
-                </li>
-            </ul>  
+                <div className={classes.mainContainer}>
+                    <div className={classes.username}>{username}</div>
+                    <ul className={classes.profileOptions}>
+                        <li>
+                            <Link to={match.url}>Images</Link>
+                        </li>
+                        <li>
+                            <Link to={`${match.url}/labels`}> Labels </Link>
+                        </li>
+                        {/* <li>
+                            <Link to={`${match.url}/settings`}> Settings </Link>
+                        </li> */}
+                    </ul>  
 
-            <Switch>
-                <Route exact path={match.path}>
-                    <UploadedImages />
-                </Route>
-                <Route path={`${match.path}/labels/:labelName`}>
-                    <SavedImages />
-                </Route>
-                <Route path={`${match.path}/labels`}>
-                    <Labels />
-                </Route>
-                <Route path={`${match.path}/settings`}>
-                    <Settings />
-                </Route>
-            </Switch>
+                    <Switch>
+                        <Route exact path={match.path}>
+                            <UploadedImages />
+                        </Route>
+                        <Route path={`${match.path}/labels/:labelName`}>
+                            <SavedImages />
+                        </Route>
+                        <Route path={`${match.path}/labels`}>
+                            <Labels />
+                        </Route>
+                        <Route path={`${match.path}/settings`}>
+                            <Settings />
+                        </Route>
+                    </Switch>
+            </div>
         </div>
     );
 }
