ad1468ec9e01d073e39a9fc3064aec18e0537d48
rishivijayv <rishivijayv@gmail.com>
2021-04-21 18:54:18 -0400

    Created a temporary gallery of images

--- src/User/UploadedImages/UploadedImages.js
+++ src/User/UploadedImages/UploadedImages.js
@@ -5,12 +5,15 @@ const useStyles = makeStyles((theme) => ({
     imageList: {
         listStyleType: 'none',
         padding: '0',
+        justifyContent: 'space-between',
         '& li': {
             display: 'inline-block',
-            marginRight: '10px',
+            marginRight: '20px',
+            marginLeft: '20px',
+            marginTop: '30px'
         },
         '& img': {
-            width: '350px',
+            maxWidth: '350px',
             height: '350px'
         }
     }
@@ -25,12 +28,13 @@ function UploadedImages(){
     return (
         <div>
             <ul className={classes.imageList}>
-                <li>
-                    <img src={tempData[0].img} />
-                </li>
-                <li>
-                    <img src={tempData[0].img} />
-                </li>
+                {tempData.map(imgData => {
+                    return (
+                        <li>
+                            <img src={imgData.img} />
+                        </li>
+                    );
+                })}
             </ul>    
         </div>
     );
@@ -41,9 +45,6 @@ const tempData = [
         img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
         title: 'Breakfast',
         author: '@bkristastucchio',
-        rows: 2,
-        cols: 2,
-        featured: true,
       },
       {
         img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
@@ -59,21 +60,16 @@ const tempData = [
         img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
         title: 'Coffee',
         author: '@nolanissac',
-        cols: 2,
       },
       {
         img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
         title: 'Hats',
         author: '@hjrc33',
-        cols: 2,
       },
       {
         img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
         title: 'Honey',
         author: '@arwinneil',
-        rows: 2,
-        cols: 2,
-        featured: true,
       },
       {
         img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
