7d89a61d6958fea704f3e53f1bd9f110949ad987
rishivijayv <rishivijayv@gmail.com>
2021-04-22 18:00:56 -0400

    Improved formatting for user saved labels

--- src/User/Labels/Labels.js
+++ src/User/Labels/Labels.js
@@ -8,10 +8,29 @@ import {
 import { labels } from '../../TempData/TempData';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    labelCard: {
+        minWidth: '400px',
+        minHeight: '340px',
+        border: '1px solid black'
+    },
+    labelContent: {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)'
+    }
+}));
 
 function Labels({ toSearch }){
     const { username } = useParams();
     const match = useRouteMatch();
+    const classes = useStyles();
 
     return (
         <div>
@@ -20,7 +39,13 @@ function Labels({ toSearch }){
                 .filter(label => label.startsWith(toSearch))
                 .map((label) => {
                     return <GridListTile>
-                        <b>~{label}~</b>
+                        <Card variant="outlined" className={classes.labelCard}>
+                            <CardContent className={classes.labelContent}>
+                                <Typography variant="h2">
+                                    ~{label}~
+                                </Typography>
+                            </CardContent>
+                        </Card>
                     </GridListTile>
                 })}
             </GridList>
