e9360b6b705ee733b5ea06784f998f7b8ef3fdb4
rishivijayv <rishivijayv@gmail.com>
2021-04-22 21:02:07 -0400

    Added basic information for image along with delete button

--- src/User/UploadedImages/UploadedImages.js
+++ src/User/UploadedImages/UploadedImages.js
@@ -1,36 +1,40 @@
 import { Link, useParams, useRouteMatch } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, fade } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 import { userUploadedImages } from '../../TempData/TempData';
+import { CardActions } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
-    imageList: {
-        listStyleType: 'none',
-        padding: '0',
-        justifyContent: 'space-between',
-        display: 'table',
-        '& li': {
-            display: 'inline-block',
-            marginRight: '20px',
-            marginLeft: '20px',
-            marginTop: '30px'
-        },
-        '& img': {
-            maxWidth: '350px',
-            height: '350px'
-        }
-    },
     gridList: {
         height: '100%',
         width: '100%'
     },
     image: {
         maxWidth: '100%',
-        height: '350px'
+        height: '450px',
+    },
+    imageCard: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    imageActions: {
+        justifyContent: 'space-between',
+        '& b':{
+            marginLeft: '10px'
+        }
+    },
+    imageActionButton: {
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.20)
+        }
     }
+    
 }));
 
 function UploadedImages({ toSearch }){
@@ -48,13 +52,19 @@ function UploadedImages({ toSearch }){
                     const lineBreak = (index % 3 == 0) ? <br /> : null;
                     return (
                         <Grid item xs={4} key={index}>
-                            <Card>
+                            <Card className={classes.imageCard}>
                                 <CardActionArea>
                                     <CardMedia 
                                         className={classes.image}
                                         image={image.img}
                                     />
                                 </CardActionArea>
+                                <CardActions className={classes.imageActions}>
+                                    <b>~{image.label}~</b>
+                                    <IconButton className={classes.imageActionButton} color="inherit">
+                                        <DeleteIcon />
+                                    </IconButton>
+                                </CardActions>
                             </Card>
                         </Grid>
                     );
