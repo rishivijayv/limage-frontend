48f9ef827cd7ddaf906b2c559f21a3401269c906
rishivijayv <rishivijayv@gmail.com>
2021-04-24 14:57:13 -0400

    Added label validation to image upload form

--- src/User/Upload/Upload.js
+++ src/User/Upload/Upload.js
@@ -3,13 +3,18 @@ import { useState, useEffect } from 'react';
 import Button from '@material-ui/core/Button';
 import PhotoCamera from '@material-ui/icons/PhotoCamera';
 import TextField from '@material-ui/core/TextField';
-import CloseIcon from '@material-ui/icons/Close';
-import IconButton from '@material-ui/core/IconButton';
+import Box from '@material-ui/core/Box';
+import PublishIcon from '@material-ui/icons/Publish';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     uploadForm: {
         textAlign: 'center'
+    },
+    buttonBox: {
+        textAlign: 'center',
+        marginLeft: 'auto',
+        marginRight: 'auto',
     },
     button: {
         color: theme.palette.common.white,
@@ -17,7 +22,8 @@ const useStyles = makeStyles((theme) => ({
         fontWeight: 'bold',
         '&:hover': {
             backgroundColor: theme.palette.common.black
-        }
+        },
+        marginRight: '10px'
     },
     label: {
         marginBottom: '10px'
@@ -54,6 +60,8 @@ function Upload(){
     const classes = useStyles();
     const [imageFile, setImageFile] = useState(null);
     const [imagePreview, setImagePreview] = useState(null);
+    const [imageLabel, setImageLabel] = useState("");
+    const [labelInvalid, setLabelInvalid] = useState(false);
 
     // Using effect to revoke object URL after component unmounts, as per https://stackoverflow.com/a/57781164
     useEffect(() => {
@@ -73,9 +81,23 @@ function Upload(){
         setImageFile(e.target.files[0]);
     };
 
+    const submitImageOnCheck = () => {
+        if(imageLabel === ""){
+            setLabelInvalid(true);
+        }else if(labelInvalid){
+            setLabelInvalid(false);
+        }
+    }
+
     return (
         <form className={classes.uploadForm}>
-            <TextField className={classes.label} label="Image Label" variant="outlined"/>
+            <TextField error={labelInvalid} 
+                    className={classes.label} 
+                    label="Image Label" 
+                    variant="outlined" 
+                    value={imageLabel} 
+                    helperText={labelInvalid ? "A label is mandatory for an image" : ""}
+                    onChange={(e) => setImageLabel(e.target.value)}/>
             <br />
             {imagePreview != null ?
             <div className={classes.previewContainer}>
@@ -83,10 +105,19 @@ function Upload(){
             </div> 
             : 
             null}
-            <Button variant="contained" className={classes.button} startIcon={<PhotoCamera />} component="label" key="image-upload-button">
-                {imagePreview != null ? "Change" : "Select"}
-                <input accept="image/*" type="file" onChange={handleImageChange} hidden />
-            </Button> 
+            <Box>
+                <Button variant="contained" className={classes.button} startIcon={<PhotoCamera />} component="label" key="image-upload-button">
+                    {imagePreview != null ? "Change" : "Select"}
+                    <input accept="image/*" type="file" onChange={handleImageChange} hidden />
+                </Button> 
+                {imagePreview != null ? 
+                <Button variant="contained" className={classes.button} startIcon={<PublishIcon />} component="label" key="image-submit-button" onClick={submitImageOnCheck}>
+                    Submit
+                </Button>
+                :
+                null}
+            </Box>
+
             <br />
 
             
