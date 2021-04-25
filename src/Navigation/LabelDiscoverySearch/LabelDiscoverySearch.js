680e6f2b01aac1e7b8ace6057a35f57d4e7f9233
rishivijayv <rishivijayv@gmail.com>
2021-04-25 14:06:48 -0400

    Removed unecessary styling from nav bar search icon

--- src/Navigation/LabelDiscoverySearch/LabelDiscoverySearch.js
+++ src/Navigation/LabelDiscoverySearch/LabelDiscoverySearch.js
@@ -6,12 +6,7 @@ import IconButton from '@material-ui/core/IconButton';
 
 const useStyles = makeStyles((theme) => ({
     search: theme.custom.search,
-    searchIcon: {
-        ...theme.custom.searchIcon,
-        '&:hover': {
-            cursor: 'pointer'
-        }
-    },
+    searchIcon: theme.custom.searchIcon,
     inputRoot: theme.custom.inputRoot,
     inputInput: theme.custom.inputInput,
 }));
