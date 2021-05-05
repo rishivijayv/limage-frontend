3a18dc74f4dfcbd8f7a234e4c7eb996c2118b50c
rishivijayv <rishivijayv@gmail.com>
2021-05-05 17:14:29 -0400

    Added empty field chceck on signup page

--- src/Landing/Signup/Signup.js
+++ src/Landing/Signup/Signup.js
@@ -1,6 +1,6 @@
 import { useHistory } from 'react-router-dom';
 import { useState } from 'react';
-import PasswordField, { initPassword, extractPassword } from '../../Utilities/PasswordField';
+import PasswordField, { initPassword } from '../../Utilities/PasswordField';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
@@ -10,22 +10,58 @@ const useStyles = makeStyles((theme) => ({
     username: {
         margin: theme.spacing(1),
         width: '25ch'
+    },
+    errorText: {
+        color: theme.palette.error.main
     }
 }));
 
 function Signup(){
     const classes = useStyles();
     const history = useHistory();
-    const [username, setUsername] = useState('');
+    const [username, setUsername] = useState({
+        text: '',
+        error: false
+    });
     const [password, setPassword] = useState(initPassword);
     const [confirmPassword, setConfirmPassword]  = useState(initPassword);
 
+    const isFieldEmpty = (field, fieldSetter) => {
+        if(field.text === ''){
+            fieldSetter({ ...field, ['error']: true });
+            return true;
+        }
+
+        return false;
+    };
+
+    const resetAllErrors = () => {
+        setUsername({ ...username, ['error']: false });
+        setPassword({ ...password, ['error']: false })
+        setConfirmPassword({ ...confirmPassword, ['error']: false })
+    };
+
+    const handleNewUserSubmit = () => {
+        resetAllErrors();
+
+        const usernameEmpty = isFieldEmpty(username, setUsername);
+        const passwordEmpty = isFieldEmpty(password, setPassword);
+        const confirmPasswordEmpty = isFieldEmpty(confirmPassword, setConfirmPassword);
+
+        if(!usernameEmpty && !passwordEmpty && !confirmPasswordEmpty){
+            history.push("/profile/rishivijayv")
+        }
+    };
+
+    const errorInForm = username.error || password.error || confirmPassword.error;
+
     return (
         <div>
-             <TextField className={classes.username} label="New Username" variant="outlined" value={username} onChange={(e) => setUsername(e.target.value)}/> <br />
-             <PasswordField passwordObject={password} passwordSetter={setPassword} labelText="Password" /> <br />
-             <PasswordField passwordObject={confirmPassword} passwordSetter={setConfirmPassword} labelText="Confirm Password" /> <br />
-             <Button className={classes.button} onClick={() => history.push("/profile/rishivijayv")} variant="container" component="label">
+             <TextField error={username.error} className={classes.username} label="New Username" variant="outlined" value={username.text} onChange={(e) => setUsername({ ...username, ['text']: e.target.value })}/> <br />
+             <PasswordField error={password.error} passwordObject={password} passwordSetter={setPassword} labelText="Password" /> <br />
+             <PasswordField error={confirmPassword.error} passwordObject={confirmPassword} passwordSetter={setConfirmPassword} labelText="Confirm Password" /> <br />
+             {errorInForm ? <h5 className={classes.errorText}>All fields are mandatory.</h5> : null}
+             <Button className={classes.button} onClick={() => handleNewUserSubmit()} variant="container" component="label">
                  Signup
              </Button>
         </div>
