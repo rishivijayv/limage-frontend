cae089e04b5b98c5867acae3c57b81c9333e03c2
rishivijayv <rishivijayv@gmail.com>
2021-05-12 16:40:52 -0400

    Adding basic types for TS

--- src/GlobalTypes.tsx
+++ src/GlobalTypes.tsx
@@ -1,11 +1,14 @@
 // A list of global types likely to be used all over the application
 
+import { OverridableComponent } from "@material-ui/core/OverridableComponent";
+import { SvgIconTypeMap } from "@material-ui/core/SvgIcon/SvgIcon";
+
 export type StateSetter<Type> = React.Dispatch<React.SetStateAction<Type>>;
 
 export type UserInputField = {
     show?: boolean,
     text: string,
-    error?: boolean
+    error: boolean
 };
 export type Image = {
     id: number,
@@ -16,4 +19,13 @@ export type Image = {
 export type HeadingLink =  {
     url: string;
     display: string;
+}
+
+export type Nullable<T> = T | null;
+
+export type Undefineable<T> = T | undefined;
+
+export type NavButton = {
+    path: string;
+    display: OverridableComponent<SvgIconTypeMap<{}, "svg">> | string;
 }
