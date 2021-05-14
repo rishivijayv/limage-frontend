56d542560872c81b5be7b0d369c55348a2304892
rishivijayv <rishivijayv@gmail.com>
2021-05-13 22:19:25 -0400

    Adding empty field error message to Settings

--- src/Settings/Security/Security.tsx
+++ src/Settings/Security/Security.tsx
@@ -3,22 +3,25 @@ import { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import PasswordField, { initPassword } from '../../Utilities/PasswordField';
 import { makeStyles, Theme } from '@material-ui/core/styles';
-import { InputField } from '../../GlobalTypes';
+import { PasswordInputField } from '../../GlobalTypes';
 import { resetError, isFieldEmpty } from '../../Utilities/HelperFunctions';
 
 
 const useStyles = makeStyles((theme: Theme) => ({
     button: theme.custom.button,
     container: theme.custom.centerContainer,
+    errorText: {
+        color: theme.palette.error.main
+    }
 }));
 
 
 function Security(){
     const classes = useStyles();
 
-    const [oldPassword, setOldPassword] = useState<InputField>(initPassword);
-    const [newPassword, setNewPassword] = useState<InputField>(initPassword);
-    const [confirmPassword, setConfirmPassword] = useState<InputField>(initPassword);
+    const [oldPassword, setOldPassword] = useState<PasswordInputField>(initPassword);
+    const [newPassword, setNewPassword] = useState<PasswordInputField>(initPassword);
+    const [confirmPassword, setConfirmPassword] = useState<PasswordInputField>(initPassword);
 
     const handleChangeRequest = () => {
         resetError(oldPassword, setOldPassword);
@@ -35,12 +38,14 @@ function Security(){
 
     };
 
+
     return (
         <div className={classes.container}>
             <h3>Change Password</h3>
             <PasswordField passwordObject={oldPassword} passwordSetter={setOldPassword} labelText="Old Password" error={oldPassword.error}/> <br />
             <PasswordField passwordObject={newPassword} passwordSetter={setNewPassword} labelText="New Password" error={newPassword.error}/> <br />
             <PasswordField passwordObject={confirmPassword} passwordSetter={setConfirmPassword} labelText="Confirm Password" error={confirmPassword.error}/> <br /> <br />
+            {oldPassword.error || newPassword.error || confirmPassword.error ? <h5 className={classes.errorText}>All fieldsa are mandatory</h5> : null}
             <Button className={classes.button} variant="contained" component="label" onClick={() => handleChangeRequest()}>
                 Change
             </Button>
